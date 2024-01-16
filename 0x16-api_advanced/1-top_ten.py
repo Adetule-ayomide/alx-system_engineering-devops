7fe43c25995199b116b4f3c7573419c8730c7f3d
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-16 13:17:25 -0800

    advanced api

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,32 +1,22 @@
 #!/usr/bin/python3
-"""
-a function that queries the Reddit API and prints the titles
-of the first 10 hot posts listed for a given subreddit
-"""
-
+"""Contains top_ten function"""
 import requests
 
 
-
 def top_ten(subreddit):
-    """a function that queries the Reddit API"""
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    
-    headers = {'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/adetule-ayomide)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-
-        response.raise_for_status()
-
-        post_data = response.json().get('data', {}).get('children', [])
-
-        for post in post_data[:10]:
-            title = post.get('data', {}).get('title', '')
-            print(title)
-
-    except requests.RequestException:
-        print(None)
-
+    """Print the titles of the 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "0x16-api_advanced:project:\
+v1.0.0 (by /u/adetule-ayomide)"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
