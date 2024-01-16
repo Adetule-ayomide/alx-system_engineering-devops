6c688b89fea91ec76d61a080368db624133b4412
Adetule-ayomide <adetuleayomide@gmail.com>
2024-01-16 08:27:04 -0800

    advanced api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,18 +7,20 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns the number of subscribers for a given subreddit."""
-    if subreddit is None or not isinstance(subreddit, str):
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {'User-Agent': '0x16-api_advanced_project/1.0.0 (by /u/adetule-ayomide)'}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        subreddit_data = response.json().get('data', {})
+        subscribers_count = subreddit_data.get('subscribers', 0)
+        return subscribers_count
+    elif response.status_code == 404:
+        print(f"The subreddit '{subreddit}' is not valid.")
+        return 0
+    else:
+        print(f"Error: {response.status_code}")
         return 0
 
-    url = f"http://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/firdaus_cartoon_jr)'}
-
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data.get("data", {}).get("subscribers", 0)
-        return subscribers
-    except requests.exceptions.RequestException:
-        return 0
